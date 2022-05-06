91864f9a7370464d27b5c4d45f0b7d4258d8c27d
armanmaulana123 <armanmaulanasaputra15@gmail.com>
2022-05-06 17:48:31 +0700

    update db

--- database/tera-c.sql
+++ database/tera-c.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 05, 2022 at 01:20 PM
+-- Generation Time: May 06, 2022 at 12:47 PM
 -- Server version: 10.1.35-MariaDB
 -- PHP Version: 7.2.9
 
@@ -51,6 +51,13 @@ CREATE TABLE `detaildatapemesanan` (
   `id_informasiStatus` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `detaildatapemesanan`
+--
+
+INSERT INTO `detaildatapemesanan` (`id_detaildataPemesanan`, `id_pembeli`, `kode_transaksi`, `total_hargaPemesanan`, `tgl_pemesanan`, `bulan_pemesanan`, `id_informasiStatus`) VALUES
+(1, 2, '060520225B6SOP', '20000', '2022-05-06', 'May', 2);
+
 -- --------------------------------------------------------
 
 --
@@ -60,10 +67,18 @@ CREATE TABLE `detaildatapemesanan` (
 CREATE TABLE `informasipemesananterasi` (
   `id_informasiPemesananTerasi` int(11) NOT NULL,
   `kode_transaksi` varchar(45) NOT NULL,
-  `id_ketersediaanTerasi` int(11) NOT NULL,
+  `kode_produk` varchar(45) NOT NULL,
   `jumlah_terasi` int(11) NOT NULL,
   `total_harga` varchar(45) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `informasipemesananterasi`
+--
+
+INSERT INTO `informasipemesananterasi` (`id_informasiPemesananTerasi`, `kode_transaksi`, `kode_produk`, `jumlah_terasi`, `total_harga`) VALUES
+(2, '060520225B6SOP', 'KP_nvX1', 2, '10000'),
+(3, '060520225B6SOP', 'KP_QF92', 1, '10000');
 
 -- --------------------------------------------------------
 
@@ -75,6 +90,18 @@ CREATE TABLE `informasistatuspemesananterasi` (
   `id_informasiStatus` int(11) NOT NULL,
   `nama_status` varchar(45) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `informasistatuspemesananterasi`
+--
+
+INSERT INTO `informasistatuspemesananterasi` (`id_informasiStatus`, `nama_status`) VALUES
+(1, 'Menunggu Pembayaran'),
+(2, 'Pengecekan Pembayaran'),
+(3, 'Pesanan Dikemas'),
+(4, 'Pembayaran Ditolak'),
+(5, 'Pesanan Dikirim'),
+(6, 'Pesanan Diterima');
 
 -- --------------------------------------------------------
 
@@ -94,6 +121,14 @@ CREATE TABLE `ketersediaanterasi` (
   `tgl_produksi` date NOT NULL,
   `tgl_restock` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `ketersediaanterasi`
+--
+
+INSERT INTO `ketersediaanterasi` (`id_ketersediaanTerasi`, `kode_produk`, `nama_terasi`, `id_penjual`, `ukuran_terasi`, `jumlah_ketersediaan`, `harga_terasi`, `foto_terasi`, `tgl_produksi`, `tgl_restock`) VALUES
+(3, 'KP_nvX1', 'Terasi 1 Kg', 1, '15', '20', '5000', 'KP_nvX1.jpg', '2022-05-05', '2022-05-05'),
+(4, 'KP_QF92', 'Terasi 2 Kg', 1, '20', '15', '10000', 'KP_QF92.jpg', '2022-05-06', '2022-05-06');
 
 -- --------------------------------------------------------
 
@@ -119,10 +154,18 @@ CREATE TABLE `laporankeuangan` (
 CREATE TABLE `pembayaranpembeliterasi` (
   `id_pembayaranPembeliTerasi` int(11) NOT NULL,
   `id_detailDataPemesanan` int(11) NOT NULL,
+  `kode_transaksi` varchar(45) NOT NULL,
   `tgl_pembayaran` date NOT NULL,
   `metode_pembayaran` varchar(45) NOT NULL,
   `bukti_pembayaran` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `pembayaranpembeliterasi`
+--
+
+INSERT INTO `pembayaranpembeliterasi` (`id_pembayaranPembeliTerasi`, `id_detailDataPemesanan`, `kode_transaksi`, `tgl_pembayaran`, `metode_pembayaran`, `bukti_pembayaran`) VALUES
+(2, 1, '060520225B6SOP', '2022-05-06', 'Transfer', 'BBB1.JPG');
 
 -- --------------------------------------------------------
 
@@ -149,7 +192,7 @@ CREATE TABLE `user` (
 
 INSERT INTO `user` (`id_user`, `nama_user`, `no_telp`, `alamat`, `email`, `password`, `jenis_kelamin`, `foto_profil`, `level_user`, `tgl_daftar`) VALUES
 (1, 'Admin Tera-C', '081234835361', 'Probolinggo', 'adminTera_c@gmail.com', '0192023a7bbd73250516f069df18b500', 'Perempuan', 'default.jpg', 1, '2022-05-04'),
-(2, 'Arman Maulana Saputra', NULL, 'Lumajang', 'user@gmail.com', '6ad14ba9986e3615423dfca256d04e3f', NULL, 'default.jpg', 2, '2022-04-14'),
+(2, 'Arman Maulana Saputra', '081234091823', 'Lumajang', 'user@gmail.com', '6ad14ba9986e3615423dfca256d04e3f', NULL, 'default.jpg', 2, '2022-04-14'),
 (3, 'Coba', '081234835352', 'coba', 'coba@gmail.com', 'a3040f90cc20fa672fe31efcae41d2db', 'Laki - Laki', 'AAA.JPG', 2, '2022-05-04');
 
 --
@@ -177,7 +220,7 @@ ALTER TABLE `detaildatapemesanan`
 --
 ALTER TABLE `informasipemesananterasi`
   ADD PRIMARY KEY (`id_informasiPemesananTerasi`),
-  ADD KEY `id_ketersediaanTerasi` (`id_ketersediaanTerasi`),
+  ADD KEY `id_ketersediaanTerasi` (`kode_produk`),
   ADD KEY `kode_transaksi` (`kode_transaksi`);
 
 --
@@ -191,7 +234,8 @@ ALTER TABLE `informasistatuspemesananterasi`
 --
 ALTER TABLE `ketersediaanterasi`
   ADD PRIMARY KEY (`id_ketersediaanTerasi`),
-  ADD KEY `id_penjual` (`id_penjual`);
+  ADD KEY `id_penjual` (`id_penjual`),
+  ADD KEY `kode_produk` (`kode_produk`);
 
 --
 -- Indexes for table `laporankeuangan`
@@ -227,25 +271,25 @@ ALTER TABLE `aduanpembeli`
 -- AUTO_INCREMENT for table `detaildatapemesanan`
 --
 ALTER TABLE `detaildatapemesanan`
-  MODIFY `id_detaildataPemesanan` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_detaildataPemesanan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `informasipemesananterasi`
 --
 ALTER TABLE `informasipemesananterasi`
-  MODIFY `id_informasiPemesananTerasi` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_informasiPemesananTerasi` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `informasistatuspemesananterasi`
 --
 ALTER TABLE `informasistatuspemesananterasi`
-  MODIFY `id_informasiStatus` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_informasiStatus` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `ketersediaanterasi`
 --
 ALTER TABLE `ketersediaanterasi`
-  MODIFY `id_ketersediaanTerasi` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id_ketersediaanTerasi` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `laporankeuangan`
@@ -257,7 +301,7 @@ ALTER TABLE `laporankeuangan`
 -- AUTO_INCREMENT for table `pembayaranpembeliterasi`
 --
 ALTER TABLE `pembayaranpembeliterasi`
-  MODIFY `id_pembayaranPembeliTerasi` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_pembayaranPembeliTerasi` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `user`
@@ -279,15 +323,13 @@ ALTER TABLE `aduanpembeli`
 -- Constraints for table `detaildatapemesanan`
 --
 ALTER TABLE `detaildatapemesanan`
-  ADD CONSTRAINT `fk_pembeliPemesanan` FOREIGN KEY (`id_pembeli`) REFERENCES `user` (`id_user`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `fk_statusPemesanan` FOREIGN KEY (`id_informasiStatus`) REFERENCES `informasistatuspemesananterasi` (`id_informasiStatus`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `fk_pembeliPemesanan` FOREIGN KEY (`id_pembeli`) REFERENCES `user` (`id_user`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `informasipemesananterasi`
 --
 ALTER TABLE `informasipemesananterasi`
-  ADD CONSTRAINT `fk_ketersediaanPemesanan` FOREIGN KEY (`id_ketersediaanTerasi`) REFERENCES `ketersediaanterasi` (`id_ketersediaanTerasi`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `fk_kodeTransaksi` FOREIGN KEY (`kode_transaksi`) REFERENCES `detaildatapemesanan` (`kode_transaksi`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `fk_kodeProduk` FOREIGN KEY (`kode_produk`) REFERENCES `ketersediaanterasi` (`kode_produk`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `ketersediaanterasi`
