18bdbcc1f0811cd6e1dc344175d0d11b1e720e8a
armanmaulana123 <armanmaulanasaputra15@gmail.com>
2022-06-16 22:36:36 +0700

    remove some fitur user

--- application/views/landing/header.php
+++ application/views/landing/header.php
@@ -199,12 +199,6 @@
                                 <a href="<?= base_url('landing/profile') ?>" class="dropdown-item has-icon">
                                     <i class="far fa-user"></i> Profile
                                 </a>
-                                <a href="features-activities.html" class="dropdown-item has-icon">
-                                    <i class="fas fa-bolt"></i> Activities
-                                </a>
-                                <a href="features-settings.html" class="dropdown-item has-icon">
-                                    <i class="fas fa-cog"></i> Settings
-                                </a>
                                 <div class="dropdown-divider"></div>
                                 <a href="<?= base_url('landing/logout') ?>" class="dropdown-item has-icon text-danger">
                                     <i class="fas fa-sign-out-alt"></i> Logout
